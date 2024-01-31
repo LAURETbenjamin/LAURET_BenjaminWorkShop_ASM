0b7a02086219eb1e57c2d158731c8ecdf5d43aa6
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-01-31 11:58:17 +0400

    puhs3

--- 5.My_Getnbr/My_Getnbr.asm
+++ 5.My_Getnbr/My_Getnbr.asm
@@ -1,5 +1,19 @@
 section .text
     global my_Getnbr
-
 my_Getnbr:
-    
+    xor rcx, rcx
+    xor rax, rax
+    xor r8, r8
+
+my_Getnbr_loop:
+    mov r8b, byte [rdi + rcx]
+    cmp r8b, 0
+    je my_end
+    sub r8b, '0'
+    imul rax, 10
+    add rax, r8
+    inc rcx
+    jmp my_Getnbr_loop
+
+my_end:
+    ret
